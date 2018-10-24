e794dfe2b78168be17689bc3fce843164107f39d
Meghan <mrj9235@rit.edu>
2018-10-24 12:44:27 -0400

    adding source project into the repository

---
+++ untitled1/src/Login.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Login extends Component{
+
+    onClickSubmit(){
+        document.getElementById();
+    }
+    render(){
+        return(
+            null
+        )
+    }
+}
