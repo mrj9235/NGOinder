af8ec344e2c02becc0c5800ce6aef8b0cdd03c5b
Meg Johnson <mrj9235@rit.edu>
2018-10-29 15:44:01 -0400

    added profile folder and classes, added path to app

--- untitled1/src/App.js
+++ untitled1/src/App.js
@@ -2,8 +2,10 @@ import React, { Component } from 'react';
 import Homepage from "./Homepage/Homepage.js";
 import Login from "./Login/Login.js";
 import Navbar from "./Navbar/Navbar.js";
+import Profile from "./Profile/Profile";
 import './App.css';
 import './Homepage/Homepage.css';
+import './Profile/Profile.css';
 import './'
 
 import {
@@ -18,9 +20,9 @@ class App extends Component {
         return (<BrowserRouter>
             <div>
                 <Navbar />
-
                 <Route exact path="/" component={Homepage} />
                 <Route path='/login' component={Login} />
+                <Route path='/profile' component={Profile}/>
             </div>
         </BrowserRouter>);
     }
