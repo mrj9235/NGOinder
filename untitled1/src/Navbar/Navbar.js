c3b656b6e2fbf672937a94e49237ec3925542cb7
Isaiah Ocasio <ijo5372@g.rit.edu>
2018-10-30 02:53:02 -0400

    Commented out and cut Marvel code for home page. Implemented new Navbar, made edits to login page

--- untitled1/src/Navbar/Navbar.js
+++ untitled1/src/Navbar/Navbar.js
@@ -4,23 +4,32 @@ class Navbar extends Component {
 
     render() {
         return (
-            <div data-collapse="medium" data-animation="default" data-duration="400" className="navigation-bar w-nav">
-                <div className="w-container" >
-                    <a href="/" className="w-nav-brand w--current">
-                        <div className="site-name">NGOinder</div>
-                    </a>
-                    <nav role="navigation" className="navigation-menu w-nav-menu"><a href="/"
-                                                                                     className="navigation-link w-nav-link w--current">Home</a><a
-                        href="/about" className="navigation-link w-nav-link">About</a><a href="/contact"
-                                                                                         className="navigation-link w-nav-link">Contact</a><a
-                        href="/login" className="nav-link-2 w-nav-link">Log In</a></nav>
-                    <div className="menu-button w-nav-button">
-                        <div className="w-icon-nav-menu"/>
-                    </div>
-                </div>
+            <div>
+                <ul>
+                    <li><a class="active" href='/home'>Home</a></li>
+                    <li><a href='/contact'>Contact</a></li>
+                    <li><a href='/about'>About</a></li>
+                    <li><a href='/login'>Log In</a></li>
+                </ul>
             </div>
         )
     }
 }
-
+/*
+<div data-collapse="medium" data-animation="default" data-duration="400" className="navigation-bar w-nav"
+<div className="w-container" >
+    <a href="/" className="w-nav-brand w--current">
+        <div className="site-name">NGOinder</div>
+    </a>
+    <nav role="navigation" className="navigation-menu w-nav-menu">
+        <a href="/" className="navigation-link w-nav-link w--current">Home</a>
+        <a href="/about" className="navigation-link w-nav-link">About</a>
+        <a href="/contact" className="navigation-link w-nav-link">Contact</a>
+        <a href="/login" className="nav-link-2 w-nav-link">Log In</a></nav>
+    <div className="menu-button w-nav-button">
+        <div className="w-icon-nav-menu"/>
+    </div>
+</div>
+</div>
+*/
 export default Navbar;
