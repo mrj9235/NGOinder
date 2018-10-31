d730a977023580174af5952efba26e9116a92f70
ASIANMANDAN <dcw2772@g.rit.edu>
2018-10-30 21:36:23 -0400

    Started work on NGO list page

---
+++ untitled1/src/List/List.js
@@ -0,0 +1,18 @@
+import React, {Componenet} from 'react';
+
+class List extends Componenet{
+
+    render(){
+        return(
+            <body>
+                //begin header
+                <h1>
+                    <span>
+                        NGOinder
+                    </span>
+                </h1>
+
+            </body>
+        )
+    }
+}
