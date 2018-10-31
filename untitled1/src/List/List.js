44a8b8e230124ff01639d08463e9532d25b5c2fb
ASIANMANDAN <dcw2772@g.rit.edu>
2018-10-30 23:24:45 -0400

    Added list of header's in alphabetical order, example NGOs in each category, and next and previous buttons

--- untitled1/src/List/List.js
+++ untitled1/src/List/List.js
@@ -11,7 +11,173 @@ class List extends Componenet{
                         NGOinder
                     </span>
                 </h1>
-
+                <h2>
+                    All NGOs
+                </h2>
+                <h3>
+                    #
+                </h3>
+                <ul type = "disc">
+                    100 Forests
+                </ul>
+                <h3>
+                    A
+                </h3>
+                <ul type = "disc">
+                    Apples Are Good
+                </ul>
+                <h3>
+                    B
+                </h3>
+                <ul type = "disc">
+                    Beavers Bro
+                </ul>
+                <h3>
+                    C
+                </h3>
+                <ul type = "disc">
+                    Cancer Sucks Foundation
+                </ul>
+                <h3>
+                    D
+                </h3>
+                <ul type = "disc">
+                    Del Children's Hospital
+                </ul>
+                <h3>
+                    E
+                </h3>
+                <ul type = "disc">
+                    Extra Life
+                </ul>
+                <h3>
+                    F
+                </h3>
+                <ul type = "disc">
+                    Fire Lovers Anonymous
+                </ul>
+                <h3>
+                    G
+                </h3>
+                <ul type = "disc">
+                    Girl Scouts is a Pyramid Scheme
+                </ul>
+                <h3>
+                    H
+                </h3>
+                <ul type = "disc">
+                    Habitat for Humanity
+                </ul>
+                <h3>
+                    I
+                </h3>
+                <ul type = "disc">
+                    I Want to Die
+                </ul>
+                <h3>
+                    J
+                </h3>
+                <ul type = "disc">
+                    Just Recycle Asshole
+                </ul>
+                <h3>
+                    K
+                </h3>
+                <ul type = "disc">
+                    Kool Kids Klub
+                </ul>
+                <h3>
+                    L
+                </h3>
+                <ul type = "disc">
+                    Lemme Smash, Becky
+                </ul>
+                <h3>
+                    M
+                </h3>
+                <ul type = "disc">
+                    Mother Nature
+                </ul>
+                <h3>
+                    N
+                </h3>
+                <ul type = "disc">
+                    Not Gonna Lie, I Hate This Project
+                </ul>
+                <h3>
+                    O
+                </h3>
+                <ul type = "disc">
+                    Oh My God, Everything's On Fire
+                </ul>
+                <h3>
+                    P
+                </h3>
+                <ul type = "disc">
+                    Please Vaccinate Your Kids
+                </ul>
+                <h3>
+                    Q
+                </h3>
+                <ul type = "disc">
+                    Quietly Dying On the Inside
+                </ul>
+                <h3>
+                    R
+                </h3>
+                <ul type = "disc">
+                    Remember When You Weren't Depressed
+                </ul>
+                <h3>
+                    S
+                </h3>
+                <ul type = "disc">
+                    Stop Being an Asshole, Love Nature
+                </ul>
+                <h3>
+                    T
+                </h3>
+                <ul type = "disc">
+                    The Dan's Running Out of Names Group
+                </ul>
+                <h3>
+                    U
+                </h3>
+                <ul type = "disc">
+                    Udder Abuse Prevention Group
+                </ul>
+                <h3>
+                    V
+                </h3>
+                <ul type = "disc">
+                    Very Quickly Approaching Critical Levels of Self Hatred
+                </ul>
+                <h3>
+                    W
+                </h3>
+                <ul type = "disc">
+                    What Would You Do For a Better President
+                </ul>
+                <h3>
+                    X
+                </h3>
+                <ul type = "disc">
+                    Xylophones Are Good for the Environment
+                </ul>
+                <h3>
+                    Y
+                </h3>
+                <ul type = "disc">
+                    You Need to Realise Climate Change is Real
+                </ul>
+                <h3>
+                    Z
+                </h3>
+                <ul type = "disc">
+                    Zoos Are Cool I Guess
+                </ul>
+                <a href="#" className="previous">&laquo; Previous</a>
+                <a href="#" className="next">Next &raquo;</a>
             </body>
         )
     }
