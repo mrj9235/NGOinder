7d7ec08e25a6e390efbec154cb3b59832ab35136
ASIANMANDAN <dcw2772@g.rit.edu>
2018-11-06 16:24:58 -0500

    Created Search page css and js, added both to App.js

--- untitled1/src/App.js
+++ untitled1/src/App.js
@@ -4,10 +4,12 @@ import Login from "./Login/Login.js";
 import Navbar from "./Navbar/Navbar.js";
 import Profile from "./Profile/Profile";
 import List from "./List/List.js";
+import Search from "./Search/Search.js";
 import './App.css';
 import './Homepage/Homepage.css';
 import './Profile/Profile.css';
 import './List/List.css';
+import './Search/Search.css';
 
 import {
     BrowserRouter,
@@ -25,6 +27,7 @@ class App extends Component {
                 <Route path='/login' component={Login} />
                 <Route path='/profile' component={Profile}/>
                 <Route path='/list' component={List}/>
+                <Route path='/search' component={Search}/>
             </div>
         </BrowserRouter>);
     }
